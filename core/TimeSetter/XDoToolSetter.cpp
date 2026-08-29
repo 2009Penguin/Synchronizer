512f1c18899d44185e03ca6b7a49d87cda1b17f8
zhan-zhehao <1474493740@qq.com>
2026-08-30 01:15:02 +0800

    Set libcurl submodule to version 7.68.0

---
+++ core/TimeSetter/XDoToolSetter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dministrator on 2026/8/30.
+//
+
+#include "XDoToolSetter.h"
---
+++ core/TimeSetter/XDoToolSetter.h
@@ -0,0 +1,16 @@
+//
+// Created by dministrator on 2026/8/30.
+//
+
+#ifndef SYNCHRONIZER_XDOTOOLSETTER_H
+#define SYNCHRONIZER_XDOTOOLSETTER_H
+
+
+
+class XDoToolSetter {
+
+};
+
+
+
+#endif //SYNCHRONIZER_XDOTOOLSETTER_H
