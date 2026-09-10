5972134b7a84e264e3018c5a616958a52db7c5b4
zhan-zhehao <1474493740@qq.com>
2026-09-10 23:19:39 +0800

    Remove inputtino submodule

---
+++ core/TimeSetter/SystemTimeSetter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dministrator on 2026/9/10.
+//
+
+#include "SystemTimeSetter.h"
---
+++ core/TimeSetter/SystemTimeSetter.h
@@ -0,0 +1,16 @@
+//
+// Created by dministrator on 2026/9/10.
+//
+
+#ifndef SYNCHRONIZER_SYSTEMTIMESETTER_H
+#define SYNCHRONIZER_SYSTEMTIMESETTER_H
+
+
+
+class SystemTimeSetter {
+
+};
+
+
+
+#endif //SYNCHRONIZER_SYSTEMTIMESETTER_H
